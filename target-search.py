c632426708626d776db210373ad94afd01ae7207
kazuoyanagimoto <kazuo.yanagimoto@gmail.com>
2015-05-23 16:01:13 +0900

    Add target_package_selected command

--- target-search.py
+++ target-search.py
@@ -2,11 +2,23 @@ import sublime, sublime_plugin
 import subprocess, webbrowser
 
 # available commands
+# target_package_selected
 # target_search_from_input
 
 def query(text):
     url = 'http://www.tyrellsys.com/' + text.replace(' ', '%20')
     webbrowser.open_new_tab(url)
+
+class TargetPackageSelectedCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            # if the user didn't select anything, search the currently highlighted word
+            if selection.empty():
+                text = self.view.word(selection)
+
+            text = self.view.substr(selection)
+
+            query(text)
 
 class TargetSearchFromInputCommand(sublime_plugin.WindowCommand):
     def run(self):
